07e27aaa37040b5953de3a0af30d0185d79c0fa0
Vrushank <vrushank1729@gmail.com>
2024-01-21 21:08:19 +0530

    hw1 submit 1

--- hw1/synthesizer/ArrayRingBuffer.java
+++ hw1/synthesizer/ArrayRingBuffer.java
@@ -1,10 +1,10 @@
 // TODO: Make sure to make this class a part of the synthesizer package
-// package <package name>;
+package synthesizer;
 import java.util.Iterator;
 
 //TODO: Make sure to make this class and all of its methods public
 //TODO: Make sure to make this class extend AbstractBoundedQueue<t>
-public class ArrayRingBuffer<T>  {
+public class ArrayRingBuffer<T> extends AbstractBoundedQueue<T> {
     /* Index for the next dequeue or peek. */
     private int first;            // index for the next dequeue or peek
     /* Index for the next enqueue. */
@@ -21,6 +21,11 @@ public class ArrayRingBuffer<T>  {
         //       this.capacity should be set appropriately. Note that the local variable
         //       here shadows the field we inherit from AbstractBoundedQueue, so
         //       you'll need to use this.capacity to set the capacity.
+        rb = (T[])new Object[capacity];
+        first = 0;
+        last = 0;
+        fillCount = 0;
+        this.capacity = capacity;
     }
 
     /**
@@ -30,6 +35,17 @@ public class ArrayRingBuffer<T>  {
      */
     public void enqueue(T x) {
         // TODO: Enqueue the item. Don't forget to increase fillCount and update last.
+        if(this.isFull()) {
+            throw new RuntimeException("It's full");
+        } else {
+            rb[last] = x;
+            fillCount += 1;
+            if(last == capacity - 1) {
+                last = 0;
+            } else {
+                last += 1;
+            }
+        }
     }
 
     /**
@@ -38,7 +54,19 @@ public class ArrayRingBuffer<T>  {
      * covered Monday.
      */
     public T dequeue() {
-        // TODO: Dequeue the first item. Don't forget to decrease fillCount and update 
+        // TODO: Dequeue the first item. Don't forget to decrease fillCount and update first.
+        if(this.isEmpty()) {
+            throw new RuntimeException("It's empty");
+        } else {
+            T firstItem = rb[first];
+            fillCount -= 1;
+            if(first == capacity - 1) {
+                first = 0;
+            } else {
+                first += 1;
+            }
+            return firstItem;
+        }
     }
 
     /**
@@ -46,6 +74,11 @@ public class ArrayRingBuffer<T>  {
      */
     public T peek() {
         // TODO: Return the first item. None of your instance variables should change.
+        if(this.isEmpty()) {
+            throw new RuntimeException("It's empty");
+        } else {
+            return rb[first];
+        }
     }
 
     // TODO: When you get to part 5, implement the needed code to support iteration.
